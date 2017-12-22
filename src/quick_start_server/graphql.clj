abab23cf373ca1ea75ad9cca868eeccd07d732ac
Yangye Zhu <yangye.zhu@wishlife.com>
2017-12-22 11:06:45 -0800

    add cameras-fn for pagination feature, cursor to be implemented

--- src/quick_start_server/graphql.clj
+++ src/quick_start_server/graphql.clj
@@ -7,6 +7,10 @@
             [wharf.core :as kebab]
             [clojure.string :as str]))
 
+(defn- uuid
+  []
+  (java.util.UUID/randomUUID))
+
 (def validated-schema (-> (io/resource "quick-start-schema.graphql")
                           slurp
                           sv/validate-schema))
@@ -40,11 +44,24 @@
 (defn name-fn [context parent args]
   (str "This is a test name" (rand-int 1000)))
 
+(def cameras (->> ["GH5", "A7RIII", "7DII", "D850", "A6300", "A6500", "X100f", "XT2", "6DII", "200D", "M100", "D7500", "A9", "RX10IV", "RX0", "GFX 50S", "X-E3", "Pentax K-P", "Leica M10", "Leica CL", "G9", "GF9", "O-MD e-m10 III", "DJI X7"]
+                  (map (fn [c]
+                         {:id (uuid)
+                          :name c}))))
+
+(defn cameras-fn [context parent args]
+  (let [{:strs [cursor limit]} args]
+    (if (not (nil? limit))
+      []
+      cameras)))
+
+
 (defn quick-start-resolver [type-name field-name]
   (get-in {:Mutation {:onClick on-click-fn}
            :Query {:hello hello-fn
                    :clicks clicks-fn
-                   :name name-fn}}
+                   :name name-fn
+                   :cameras cameras-fn}}
           [(keyword type-name) (keyword field-name)]
           (if (not (or (str/starts-with? field-name "__")
                        (str/starts-with? type-name "__")))
